a3b180d21584274d33047f0841414cf4e7386574
OlgaStam <newrealityforme@gmail.com>
2024-09-15 20:41:25 +0300

    ошибки диплоя

--- src/Container/App/App.tsx
+++ src/Container/App/App.tsx
@@ -3,12 +3,12 @@ import { StyledEngineProvider } from '@mui/material'
 import Container from '@mui/material/Container'
 import Header from '../Header/Header'
 import Footer from '../Footer/Footer'
-import Home from 'Pages/Home/Home'
 import Contact from 'Pages/Contact/Contact'
 import Price from 'Pages/Price/Price'
 import Serve from 'Pages/Serve/Serve'
 import './App.scss'
 import React from 'react'
+import Home from 'Pages/Home/Home'
 // type Props = {}
 const App = () => {
     return (
@@ -16,8 +16,8 @@ const App = () => {
             <Header />
             <Container>
                 <Routes>
-                    <Route path="/" element={<Home />} />
                     <Route path="/Contact" element={<Contact />} />
+                    <Route path="/" element={<Home />} />
                     <Route path="/Price" element={<Price />} />
                     <Route path="/Serve" element={<Serve />} />
                 </Routes>
