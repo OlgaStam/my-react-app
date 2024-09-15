93f340e4ac50bab7758c23f8392738cc518bed54
OlgaStam <newrealityforme@gmail.com>
2024-09-15 21:35:04 +0300

    ошибки диплоя

--- src/Container/App/App.tsx
+++ src/Container/App/App.tsx
@@ -1,22 +1,22 @@
+import React from 'react'
+import { StyledEngineProvider } from '@mui/material'
+import { Routes, Route } from 'react-router-dom'
 import Container from '@mui/material/Container'
 import Header from '../Header/Header'
 import Footer from '../Footer/Footer'
-import Contact from 'Pages/Contact/Contact'
-import Price from 'Pages/Price/Price'
-import Serve from 'Pages/Serve/Serve'
+import Home from '../../Pages/Home/Home'
+import Contact from '../../Pages/Contact/Contact'
+import Price from '../../Pages/Price/Price'
+import Serve from '../../Pages/Serve/Serve'
 import './App.scss'
-import Home from 'Pages/Home/Home'
-import { StyledEngineProvider } from '@mui/material'
-import { Routes, Route } from 'react-router-dom'
-import React from 'react'
 const App = () => {
     return (
         <StyledEngineProvider injectFirst>
             <Header />
             <Container>
                 <Routes>
-                    <Route path="/Contact" element={<Contact />} />
                     <Route path="/" element={<Home />} />
+                    <Route path="/Contact" element={<Contact />} />
                     <Route path="/Price" element={<Price />} />
                     <Route path="/Serve" element={<Serve />} />
                 </Routes>
