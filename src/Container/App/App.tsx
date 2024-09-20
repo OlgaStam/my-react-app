4d50605b3732a37ae43b202b8e9eddd5d1786ca9
OlgaStam <newrealityforme@gmail.com>
2024-09-20 19:50:15 +0300

    ошибки диплоя
    заменить алиасы на относительные пути, чтобы убедиться, что проблема именно в алиасах

--- src/Container/App/App.tsx
+++ src/Container/App/App.tsx
@@ -4,11 +4,10 @@ import { Routes, Route } from 'react-router-dom'
 import Container from '@mui/material/Container'
 import Header from '../Header/Header'
 import Footer from '../Footer/Footer'
-import Home from '@Pages/Home/Home'
-
-import Contact from '@Pages/Contact/Contact'
-import Price from '@Pages/Price/Price'
-import Serve from '@Pages/Serve/Serve'
+import Home from '../../Pages/Home/Home' // Измени путь в зависимости от структуры
+import Contact from '../../Pages/Contact/Contact'
+import Price from '../../Pages/Price/Price'
+import Serve from '../../Pages/Serve/Serve'
 import './App.scss'
 const App = () => {
     return (
