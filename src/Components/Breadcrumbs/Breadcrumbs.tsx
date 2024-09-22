e361bda4ca5e3122d0e917dec2c5ef1d1f1d31bf
OlgaStam <newrealityforme@gmail.com>
2024-09-22 14:18:18 +0300

    сопоставление пути (route) с украинским названием страницы

--- src/Components/Breadcrumbs/Breadcrumbs.tsx
+++ src/Components/Breadcrumbs/Breadcrumbs.tsx
@@ -2,6 +2,15 @@ import React from 'react'
 import { Breadcrumbs, Link, Typography } from '@mui/material'
 import { useLocation, Link as RouterLink } from 'react-router-dom'
 
+// Объект для сопоставления пути с украинским названием страницы
+const breadcrumbNameMap: { [key: string]: string } = {
+    '/': 'Головна',
+    '/Contact': 'Контакти',
+    '/Price': 'Ціни',
+    '/Serve': 'Послуги',
+    // Добавь сюда другие страницы
+}
+
 const AppBreadcrumbs: React.FC = () => {
     const location = useLocation()
     const pathnames = location.pathname.split('/').filter((x) => x)
@@ -26,15 +35,18 @@ const AppBreadcrumbs: React.FC = () => {
                             style={{ marginRight: 4 }}
                         ></i>{' '}
                         {/* Иконка домика */}
-                        Головна
+                        {breadcrumbNameMap['/']}
                     </Link>
                     {pathnames.map((name, index) => {
                         const routeTo = `/${pathnames.slice(0, index + 1).join('/')}`
                         const isLast = index === pathnames.length - 1
+                        const translatedName =
+                            breadcrumbNameMap[routeTo] ||
+                            name.charAt(0).toUpperCase() + name.slice(1)
 
                         return isLast ? (
                             <Typography key={routeTo} color="textPrimary">
-                                {name.charAt(0).toUpperCase() + name.slice(1)}
+                                {translatedName}
                             </Typography>
                         ) : (
                             <Link
@@ -44,7 +56,7 @@ const AppBreadcrumbs: React.FC = () => {
                                 color="inherit"
                                 style={{ textDecoration: 'none' }}
                             >
-                                {name.charAt(0).toUpperCase() + name.slice(1)}
+                                {translatedName}
                             </Link>
                         )
                     })}
