34f352549471fb4b0bbe8ce1eeb3d591e62f3a47
OlgaStam <newrealityforme@gmail.com>
2024-09-22 20:32:27 +0300

    прокрутку модалке для маленького окна

--- src/Container/App/App.tsx
+++ src/Container/App/App.tsx
@@ -5,7 +5,7 @@ import { Routes, Route } from 'react-router-dom'
 import Container from '@mui/material/Container'
 import Header from '../Header/Header'
 import Footer from '../Footer/Footer'
-import Home from '../../Pages/Home/Home' // Измени путь в зависимости от структуры
+import Home from '../../Pages/Home/Home'
 import Contact from '../../Pages/Contact/Contact'
 import Price from '../../Pages/Price/Price'
 import Serve from '../../Pages/Serve/Serve'
